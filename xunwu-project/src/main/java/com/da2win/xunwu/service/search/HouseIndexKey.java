c42bd12d32f6d21630e842a01b6db3a5d44381c8
Wenwen <da2win@foxmail.com>
2018-07-09 16:41:50 +0800

    构建es索引

---
+++ xunwu-project/src/main/java/com/da2win/xunwu/service/search/HouseIndexKey.java
@@ -0,0 +1,12 @@
+package com.da2win.xunwu.service.search;
+
+/**
+ * 索引关键词统一定义
+ *
+ * @author Darwin
+ * @date 2018/7/9
+ */
+public class HouseIndexKey {
+    public static final String HOUSE_ID = "houseId";
+    public static final String TITLE = "title";
+}
